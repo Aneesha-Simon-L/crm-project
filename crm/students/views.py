55f9236acc061fcce1d1ad0710a20f2117b45ea5
Aneesha-Simon-L <aneeshasimon77@gmail.com>
2025-02-26 08:39:31 +0000

    added

--- crm/students/views.py
+++ crm/students/views.py
@@ -55,11 +55,19 @@ class StudentsListView(View):
 
         query = request.GET.get('query')
 
-        all_students = StudentsView.objects.filter(active_status = True)
+        role  = request.user.role
+
+        if role in ['Trainer']:
+
+            all_students = StudentsView.objects.filter(active_status = True,trainer__profile = request.user)
+
+        else:
+
+            all_students = StudentsView.objects.filter(active_status = True)
 
         if query:
 
-            all_students = StudentsView.objects.filter(Q(active_status = True)&(Q(first_name__icontains = query)|Q(last_name__icontains = query)|
+            all_students = StudentsView.objects.filter(Q(active_status = True)&Q(trainer__profile = request.user)&(Q(first_name__icontains = query)|Q(last_name__icontains = query)|
                                                                                 Q(email__icontains = query)|Q(contact_num__icontains = query)|
                                                                                 Q(house_name__icontains = query)|Q(pincode__icontains = query)|
                                                                                 Q(course__name__icontains = query)|Q(batch__name__icontains = query)))
